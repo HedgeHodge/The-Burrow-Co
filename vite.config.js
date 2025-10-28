1ad930139e8760dcec0547ccea115548adc70141
Tyler Hodges <Hodges3902@gmail.com>
2025-10-28 16:51:00 -0700

    Add built files

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "node:path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src")
-    }
-  },
-  base: "./"
-});
+  base: '/', // or '/repo-name/' if using project pages
+  build: {
+    outDir: 'docs', // Change output from 'dist' to 'docs'
+    emptyOutDir: true,
+  }
+})
