95dec0a28ea95392f67bfc4ab7010ae8973fa781
Tyler Hodges <Hodges3902@gmail.com>
2025-10-28 16:57:14 -0700

    Update vite.config.js to include path alias configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/', // Use '/' since you have a custom domain
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
