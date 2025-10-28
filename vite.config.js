7fd75e98c8607cfae7c432a6e0512846dc39ef8d
Tyler Hodges <Hodges3902@gmail.com>
2025-10-28 16:53:24 -0700

    Add GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // or '/repo-name/' if using project pages
-  build: {
-    outDir: 'docs', // Change output from 'dist' to 'docs'
-    emptyOutDir: true,
-  }
+  base: '/', // Use '/' since you have a custom domain
 })
